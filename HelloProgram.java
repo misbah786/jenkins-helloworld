e19c9a832f9fa4e55580282799b5b93d53b3c4cf
misbah786 <misbah786shaik@gmail.com>
2021-02-16 13:38:57 +0530

    add hello world program

--- HelloProgram.java
+++ HelloProgram.java
@@ -1,7 +1,7 @@
 public class HelloProgram{
   public static void main(String[] s){
-          System.out.println("Loop based execution");
-          for(int i=0;i<10;i++){
+          System.out.println("Loop based execution -CICD Demo");
+          for(int i=0;i<50;i++){
               System.out.println(i);
 }
 }
